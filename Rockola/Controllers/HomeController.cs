a3a4020e375a3fe249e7b71d8b77b8a1b78f78e1
briseidy-torres-velez <58743853+briseidy-torres-velez@users.noreply.github.com>
2020-01-20 15:27:36 -0600

    20ene

---
+++ Rockola/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using Newtonsoft.Json;
+
+namespace Rockola.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SearchList(string keyword)
+        {
+            var youtuberService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyBA5MWGsEof8dVHxcyJv3TYnP-qSRh1vjk",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtuberService.Search.List("snippet");
+            searchListRequest.Q = keyword;
+
+
+            searchListRequest.MaxResults = 5;
+
+
+
+            var searchListResponse = searchListRequest.Execute();
+
+            var respuesta = searchListResponse.Items;
+
+            
+
+            return PartialView("Search",searchListResponse.Items);
+        }
+
+
+        //[HttpGet]
+        //public JsonResult PlayList(Google.Apis.YouTube.v3.Data.SearchResult VideoId)
+        //{
+
+        //    return Json(VideoId, JsonRequestBehavior.AllowGet);
+
+        //  //  return PartialView("AddPlay",Json(VideoId, JsonRequestBehavior.AllowGet));
+        //}
+
+
+    }
+}
